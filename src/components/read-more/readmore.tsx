60a212d231c9e2c5d50558d90ac5e1568a842480
kumarohan0115 <rohankr350@gmail.com>
2024-05-09 14:01:18 +0530

    Responsiveness update

--- src/components/read-more/readmore.tsx
+++ src/components/read-more/readmore.tsx
@@ -10,13 +10,13 @@ const ReadMore:React.FC<Props> = ({companyDetails,setReadMore}) => {
 
   return (
     <div className='lg:w-[100vw] lg:h-[100vh] backdrop-blur-sm z-10 fixed inset-0 flex items-center justify-center bg-alpa_bg_color'>
-        <div className='sm:w-[90%] lg:w-[70%] lg:h-[80%]  bg-bg_color shadow-md shadow-shadow_color/20'>
-            <div className='heading-wrapper flex justify-between p-8 border-b-2 relative'>
-                <span className='absolute right-4 top-2 cursor-pointer' onClick={()=>setReadMore(false)}><Icon name={'cross'} customClass={'whiteIcon'}/></span>
-                <div className='w-[50%] flex flex-col text-left'>
+        <div className='sm:w-[100%] lg:w-[70%] lg:h-[80%]  bg-bg_color shadow-md shadow-shadow_color/20'>
+            <div className='heading-wrapper flex justify-between p-8 border-b-2 relative sm:flex-row flex-col-reverse'>
+                <span className='absolute right-4 sm:top-2 top-8 cursor-pointer' onClick={()=>setReadMore(false)}><Icon name={'cross'} customClass={'whiteIcon'}/></span>
+                <div className='sm:w-[50%] w-full flex flex-col text-left'>
                     <h2 className="text-3xl font-bold">{companyDetails?.name}</h2>
                     <h3>{companyDetails?.designation}</h3>
-                    <div className='flex gap-2 mt-4'>
+                    <div className='flex gap-2 flex-wrap mt-4'>
                     {
                         companyDetails?.techStack?.map((ele: any,ind: number)=>{
                             return <li key={ind} className='list-none bg-[#888] px-4 rounded-sm whitespace-nowrap' >{ele}</li>
